c38b8d593e1917e55a53e65d6dc7f7d99613c98f
Magno de Oliveira Paz <magno01@gmail.com.br>
2021-04-24 13:09:19 -0300

    ...

--- src/entities/sites.py
+++ src/entities/sites.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from chromedriver_py import binary_path
-from entities import petz
 
 
 class Site:
     name: str
     base_url: str
     driver: WebDriver
+    suffix: list[str]
 
-    def __init__(self, name: str, base_url: str):
+    def __init__(self, name: str, base_url: str, suffix):
         self.name = name
         self.base_url = base_url
         self.driver = webdriver.Chrome(executable_path=binary_path)
+        self.suffix = suffix
 
     def scrap():
         pass
@@ -23,9 +24,9 @@ class Petz(Site):
     def __init__(self) -> None:
         super().__init__("Petz", "https://www.petz.com.br/")
 
-    def scrap():
-        super.self.driver.get(super.self.base_url)
-        super.self.driver.close()
+    def scrap(self):
+        self.driver.get(self.base_url)
+        self.driver.close()
 
 
 def sites_list():
