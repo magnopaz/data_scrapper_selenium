0dfd324ef510decfd69b131292d4be15a6b21e93
Magno de Oliveira Paz <magno01@gmail.com.br>
2021-04-28 13:40:50 -0300

    Correções diversas

--- src/entities/petz.py
+++ src/entities/petz.py
@@ -1,9 +1,9 @@
-from entities import site
+from entities import base_site as site
 
 
 class Petz(site.Site):
     def __init__(self) -> None:
-        super().__init__("Petz", "https://www.petz.com.br/")
+        super().__init__("Petz", "https://www.petz.com.br/", "")
 
     def scrap(self):
         self.driver.get(self.base_url)
